616494f13c80ed5e3a18ee67f0aa0836c3d74be9
Svitlana Lesiv <lesivsvitlana@gmail.com>
2019-02-28 17:42:00 -0800

    fix identation

--- js/form_validation.js
+++ js/form_validation.js
@@ -12,201 +12,201 @@
  * @version Last_modified 13_Dec_2017
  **/
 $(document).ready(function() {
-    "use strict";
+  "use strict";
 
-    /*Form validation*
-     *Capturing and handling events*
-     *DOM element creation, deletion or modification*
-     *****************************************/
+  /*Form validation*
+    *Capturing and handling events*
+    *DOM element creation, deletion or modification*
+    *****************************************/
 
-    /**
-     * this function writes to <span> element value of input
-     * field for name; and show/hide hint message
-     **/
-    $(name1).keyup(function() {
-        //if any value in input
-        if ($(this).val()) {
-            // write that value to <span> and hide hint message
-            $(nameAva).html($(this).val());
-            $(nameHint).css("display", "none");
-        } else {
-            //if not write to <span> - "Your name here" and show hint
-            $(nameAva).html("Your name here");
-            $(nameHint).css("display", "block");
-        }
-    });
-
-
-    //source of regex:
-    //https://stackoverflow.com/questions/201323/using-a-regular-expression-to-validate-an-email-address
-    var emailReg = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
-
-    /**
-     * this function validates email address and show/hide hint message
-     **/
-    $(email1).keyup(function() {
-        //if value of email input field doesn't match with regex
-        if (!$(this).val().match(emailReg)) {
-            //show warning message
-            $(email1Hint).css("display", "block");
-        } else {
-            $(email1Hint).css("display", "none");
-        }
-    });
-
-    /**
-     * this function validates password input field,
-     * and shows hint how strong password by calling
-     * passStrenghtHint(),passLength(),isUper(),isSpecialCh(),isNum() functions
-     *
-     * they are stored in password_validation.js file
-     **/
-    $(password1).keyup(function() {
-        // show hint message if password doesn't have:
-        //uppercase,special character, number and also show password strenght
-        passStrenghtHint();
-        //add one hyphen to password strenght if password has 8 character
-        isLong();
-        //add one hyphen to password strenght if password has one uppercase
-        isUper();
-        //add one hyphen to password strenght if password has one special character
-        isSpecialCh();
-        //add one hyphen to password strenght if password has one number
-        isNum();
-    });
-
-    /**
-     * this function checks if password value
-     * from first field matches with second field and
-     * shows warning if they don't match
-     **/
-    $(password2).keyup(function() {
-        if ($(this).val().trim() != $(password1).val().trim()) {
-            $(pwd2Hint).css("display", "block");
-        } else {
-            $(pwd2Hint).css("display", "none");
-        }
-    });
-
-    /*Creating and handling a data structure*
-     *****************************************/
-    //images source: http://www.freepik.com
-    // create an object for avatars
-    var avatarsList = {
-        "avatars": [{
-                "avatarName": "boy",
-                "avatarPic": "./img/boy.png"
-            },
-            {
-                "avatarName": "girl",
-                "avatarPic": "./img/girl.png"
-            },
-            {
-                "avatarName": "cat",
-                "avatarPic": "./img/cat.png"
-            },
-            {
-                "avatarName": "chicken",
-                "avatarPic": "./img/chicken.png"
-            },
-            {
-                "avatarName": "cow",
-                "avatarPic": "./img/cow.png"
-            },
-            {
-                "avatarName": "deer",
-                "avatarPic": "./img/deer.png"
-            },
-            {
-                "avatarName": "fox",
-                "avatarPic": "./img/fox.png"
-            },
-            {
-                "avatarName": "monkey",
-                "avatarPic": "./img/monkey.png"
-            },
-            {
-                "avatarName": "panda",
-                "avatarPic": "./img/panda.png"
-            },
-            {
-                "avatarName": "pig",
-                "avatarPic": "./img/pig.png"
-            }
-        ]
-    };
-
-    /**
-     * this function creates <option> for avatarName
-     * and appends to <select>
-     * */
-    function populateSelectElement(avatarsList) {
-        for (var i = 0; i < avatarsList.avatars.length; i++) {
-            for (var key in avatarsList.avatars[i]) {
-                // look for 'avatarName' as a key
-                if (key == "avatarName") {
-                    var s = document.createElement("option");
-                    var t = document.createTextNode(avatarsList.avatars[i][key]);
-                    // add text node to <option>
-                    $(s).append(t);
-                    // set value="" on the <option>
-                    $(s).attr("value", avatarsList.avatars[i][key]);
-                    // add the new <option> to the <select>
-                    $(avatarSel).append(s);
-                }
-            }
-        }
+  /**
+   * this function writes to <span> element value of input
+   * field for name; and show/hide hint message
+   **/
+  $(name1).keyup(function() {
+    //if any value in input
+    if ($(this).val()) {
+      // write that value to <span> and hide hint message
+      $(nameAva).html($(this).val());
+      $(nameHint).css("display", "none");
+    } else {
+      //if not write to <span> - "Your name here" and show hint
+      $(nameAva).html("Your name here");
+      $(nameHint).css("display", "block");
     }
+  });
 
-    populateSelectElement(avatarsList);
 
-    /**
-     * this function change images in <aside>, depending
-     * what is selected in drop-down list
-     **/
-    $(avatarSel).change(function() {
-        //if something selected in <select>
-        if ($(avatarSel).val()) {
-            //img source variable
-            var imgSrc = "./img/" + $(avatarSel).val() + ".png"
-            //change img source in <img>
-            $(pic).attr("src", imgSrc);
-            //change color of <option> if selected
-            $("select").css("color", "white");
-            $("select").css("text-decoration", "none");
-        } else {
-            //if nothing selected, keep default image
-            $(pic).attr("src", "img/girl.png");
-            $("select").css("color", "#999");
+  //source of regex:
+  //https://stackoverflow.com/questions/201323/using-a-regular-expression-to-validate-an-email-address
+  var emailReg = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+
+  /**
+   * this function validates email address and show/hide hint message
+   **/
+  $(email1).keyup(function() {
+    //if value of email input field doesn't match with regex
+    if (!$(this).val().match(emailReg)) {
+      //show warning message
+      $(email1Hint).css("display", "block");
+    } else {
+      $(email1Hint).css("display", "none");
+    }
+  });
+
+  /**
+   * this function validates password input field,
+   * and shows hint how strong password by calling
+   * passStrenghtHint(),passLength(),isUper(),isSpecialCh(),isNum() functions
+   *
+   * they are stored in password_validation.js file
+   **/
+  $(password1).keyup(function() {
+    // show hint message if password doesn't have:
+    //uppercase,special character, number and also show password strenght
+    passStrenghtHint();
+    //add one hyphen to password strenght if password has 8 character
+    isLong();
+    //add one hyphen to password strenght if password has one uppercase
+    isUper();
+    //add one hyphen to password strenght if password has one special character
+    isSpecialCh();
+    //add one hyphen to password strenght if password has one number
+    isNum();
+  });
+
+  /**
+   * this function checks if password value
+   * from first field matches with second field and
+   * shows warning if they don't match
+   **/
+  $(password2).keyup(function() {
+    if ($(this).val().trim() != $(password1).val().trim()) {
+      $(pwd2Hint).css("display", "block");
+    } else {
+      $(pwd2Hint).css("display", "none");
+    }
+  });
+
+  /*Creating and handling a data structure*
+    *****************************************/
+  //images source: http://www.freepik.com
+  // create an object for avatars
+  var avatarsList = {
+    "avatars": [{
+        "avatarName": "boy",
+        "avatarPic": "./img/boy.png"
+      },
+      {
+        "avatarName": "girl",
+        "avatarPic": "./img/girl.png"
+      },
+      {
+        "avatarName": "cat",
+        "avatarPic": "./img/cat.png"
+      },
+      {
+        "avatarName": "chicken",
+        "avatarPic": "./img/chicken.png"
+      },
+      {
+        "avatarName": "cow",
+        "avatarPic": "./img/cow.png"
+      },
+      {
+        "avatarName": "deer",
+        "avatarPic": "./img/deer.png"
+      },
+      {
+        "avatarName": "fox",
+        "avatarPic": "./img/fox.png"
+      },
+      {
+        "avatarName": "monkey",
+        "avatarPic": "./img/monkey.png"
+      },
+      {
+        "avatarName": "panda",
+        "avatarPic": "./img/panda.png"
+      },
+      {
+        "avatarName": "pig",
+        "avatarPic": "./img/pig.png"
+      }
+    ]
+  };
+
+  /**
+   * this function creates <option> for avatarName
+   * and appends to <select>
+   * */
+  function populateSelectElement(avatarsList) {
+    for (var i = 0; i < avatarsList.avatars.length; i++) {
+      for (var key in avatarsList.avatars[i]) {
+        // look for 'avatarName' as a key
+        if (key == "avatarName") {
+          var s = document.createElement("option");
+          var t = document.createTextNode(avatarsList.avatars[i][key]);
+          // add text node to <option>
+          $(s).append(t);
+          // set value="" on the <option>
+          $(s).attr("value", avatarsList.avatars[i][key]);
+          // add the new <option> to the <select>
+          $(avatarSel).append(s);
         }
-    });
+      }
+    }
+  }
 
-    /**
-     * this function prevents from submitting if
-     * there are no value in specified input field or value is not valid
-     **/
-    $(submitBtn).click(function(e) {
-        if (!$(name).val()) {
-            // to prevent submission to the sever if conditions haven't met
-            e.preventDefault();
-            // write warning message
-            alert("Please write your name");
-            //check if email input is not empty and if it's valid
-        } else if (!$(email).val() || !$(email).val().match(emailReg)) {
-            e.preventDefault();
-            alert("Please write your email");
-            //check if password field not empty and if has at least 8 characters
-        } else if (!$(password1).val() || !$(password1).val().match(passReg)) {
-            e.preventDefault();
-            alert("Your password should be at least 8 characters long");
-            //check password match field: if not empty and if valid
-        } else if (!$(password2).val() || $(password2).val().trim() != $(password1).val().trim()) {
-            e.preventDefault();
-            alert("Retype your password correctly");
-        } else {
-            //if everything is good - submit form
-            $(regForm).submit();
-        }
-    });
+  populateSelectElement(avatarsList);
+
+  /**
+   * this function change images in <aside>, depending
+   * what is selected in drop-down list
+   **/
+  $(avatarSel).change(function() {
+    //if something selected in <select>
+    if ($(avatarSel).val()) {
+      //img source variable
+      var imgSrc = "./img/" + $(avatarSel).val() + ".png"
+      //change img source in <img>
+      $(pic).attr("src", imgSrc);
+      //change color of <option> if selected
+      $("select").css("color", "white");
+      $("select").css("text-decoration", "none");
+    } else {
+      //if nothing selected, keep default image
+      $(pic).attr("src", "img/girl.png");
+      $("select").css("color", "#999");
+    }
+  });
+
+  /**
+   * this function prevents from submitting if
+   * there are no value in specified input field or value is not valid
+   **/
+  $(submitBtn).click(function(e) {
+    if (!$(name).val()) {
+      // to prevent submission to the sever if conditions haven't met
+      e.preventDefault();
+      // write warning message
+      alert("Please write your name");
+      //check if email input is not empty and if it's valid
+    } else if (!$(email).val() || !$(email).val().match(emailReg)) {
+      e.preventDefault();
+      alert("Please write your email");
+      //check if password field not empty and if has at least 8 characters
+    } else if (!$(password1).val() || !$(password1).val().match(passReg)) {
+      e.preventDefault();
+      alert("Your password should be at least 8 characters long");
+      //check password match field: if not empty and if valid
+    } else if (!$(password2).val() || $(password2).val().trim() != $(password1).val().trim()) {
+      e.preventDefault();
+      alert("Retype your password correctly");
+    } else {
+      //if everything is good - submit form
+      $(regForm).submit();
+    }
+  });
 
 });
--- js/graph.js
+++ js/graph.js
@@ -14,190 +14,190 @@
  * @version Last_modified 13_Dec_2017
  **/
 $(document).ready(function() {
-    /*DOM element creation, deletion or modification*
-     *****************************************/
-    //create canvas element
-    "use strict";
-    var canvas = document.getElementById("c1");
-    var ctx = canvas.getContext('2d');
-    var w = canvas.width;
-    var h = canvas.height;
+  /*DOM element creation, deletion or modification*
+    *****************************************/
+  //create canvas element
+  "use strict";
+  var canvas = document.getElementById("c1");
+  var ctx = canvas.getContext('2d');
+  var w = canvas.width;
+  var h = canvas.height;
 
+  horizLine();
+
+  /**
+   * this function will draw horizontal lines on canvas element
+   */
+  function horizLine() {
+    ctx.strokeStyle = "#bff698";
+    ctx.lineWidth = 0.2;
+    //draw lines with the step of 10 till it reaches end of canvas height
+    for (var i = 0; i < h; i += 10) {
+      ctx.beginPath();
+      ctx.moveTo(0, i);
+      ctx.lineTo(w, i);
+      ctx.stroke();
+    }
+  }
+
+  /**
+   * this function draws one rectangle on a canvas
+   * */
+  function buildOneGraph(x, y) {
+    ctx.fillStyle = "#bff698";
+    ctx.beginPath();
+    ctx.moveTo(x + 10, h - y);
+    ctx.lineTo(x + 50, h - y);
+    ctx.lineTo(x + 50, h);
+    ctx.lineTo(x + 10, h);
+    ctx.closePath();
+    ctx.fill();
+  }
+
+  /**
+   * this function calls buildOneGraph() function to
+   * draw graphs for one region
+   * */
+  function dataDisplay(region) {
+    //x = 0; y = data for 1995 year of current region
+    buildOneGraph(0, region.getY1995());
+    //x = 60; y = data for 2000 year of current region
+    buildOneGraph(60, region.getY2000());
+    buildOneGraph(120, region.getY2010());
+    buildOneGraph(180, region.getY2014());
+    buildOneGraph(240, region.getY2020());
+  }
+
+  /*Capturing and handling events*
+    *****************************************/
+  /**
+   * this function checks if any changes in <select> element;
+   * if yes, and if we selected some <option>, calls dataDisplay()
+   * to display data for selected region
+   * */
+  $(rgnSelect).change(function() {
+    //clear canvas
+    ctx.clearRect(0, 0, w, h);
+    //draw horizontal lines
     horizLine();
 
-    /**
-     * this function will draw horizontal lines on canvas element
-     */
-    function horizLine() {
-        ctx.strokeStyle = "#bff698";
-        ctx.lineWidth = 0.2;
-        //draw lines with the step of 10 till it reaches end of canvas height
-        for (var i = 0; i < h; i += 10) {
-            ctx.beginPath();
-            ctx.moveTo(0, i);
-            ctx.lineTo(w, i);
-            ctx.stroke();
-        }
+    //<option> value
+    var val = this.options[this.selectedIndex].value;
+    if (val >= 0) {
+      //array regions[index(which is <option> value)]
+      var rgn = regions[val];
+      //change color of selected option to white
+      $("select").css("color", "white");
+      $("select").css("text-decoration", "none");
+      //show years for graphs under canvas
+      $(".year").css("visibility", "visible");
+      //display data on canvas
+      dataDisplay(rgn);
+    } else {
+      $("select").css("color", "#999");
+      $(".year").css("visibility", "hidden");
     }
+  });
 
-    /**
-     * this function draws one rectangle on a canvas
-     * */
-    function buildOneGraph(x, y) {
-        ctx.fillStyle = "#bff698";
-        ctx.beginPath();
-        ctx.moveTo(x + 10, h - y);
-        ctx.lineTo(x + 50, h - y);
-        ctx.lineTo(x + 50, h);
-        ctx.lineTo(x + 10, h);
-        ctx.closePath();
-        ctx.fill();
-    }
+  /**
+   * this function creates <option> for each region
+   * and appends to <select>
+   * */
+  function populateSelectElement(regions) {
+    regions.forEach(function(region, index) {
+      //for each region get its name
+      var name = region.getName();
+      //create <option>
+      var option = document.createElement("option");
 
-    /**
-     * this function calls buildOneGraph() function to
-     * draw graphs for one region
-     * */
-    function dataDisplay(region) {
-        //x = 0; y = data for 1995 year of current region
-        buildOneGraph(0, region.getY1995());
-        //x = 60; y = data for 2000 year of current region
-        buildOneGraph(60, region.getY2000());
-        buildOneGraph(120, region.getY2010());
-        buildOneGraph(180, region.getY2014());
-        buildOneGraph(240, region.getY2020());
-    }
+      //assign index to option value
+      option.value = index;
+      //create text node - which is name of region
+      option.appendChild(document.createTextNode(name));
 
-    /*Capturing and handling events*
-     *****************************************/
-    /**
-     * this function checks if any changes in <select> element;
-     * if yes, and if we selected some <option>, calls dataDisplay()
-     * to display data for selected region
-     * */
-    $(rgnSelect).change(function() {
-        //clear canvas
-        ctx.clearRect(0, 0, w, h);
-        //draw horizontal lines
-        horizLine();
-
-        //<option> value
-        var val = this.options[this.selectedIndex].value;
-        if (val >= 0) {
-            //array regions[index(which is <option> value)]
-            var rgn = regions[val];
-            //change color of selected option to white
-            $("select").css("color", "white");
-            $("select").css("text-decoration", "none");
-            //show years for graphs under canvas
-            $(".year").css("visibility", "visible");
-            //display data on canvas
-            dataDisplay(rgn);
-        } else {
-            $("select").css("color", "#999");
-            $(".year").css("visibility", "hidden");
-        }
+      //append <option> to <select>
+      $(rgnSelect).append(option);
     });
+  }
 
-    /**
-     * this function creates <option> for each region
-     * and appends to <select>
-     * */
-    function populateSelectElement(regions) {
-        regions.forEach(function(region, index) {
-            //for each region get its name
-            var name = region.getName();
-            //create <option>
-            var option = document.createElement("option");
+  /*Creating and handling a data structure*
+    *****************************************/
+  var regions = [];
 
-            //assign index to option value
-            option.value = index;
-            //create text node - which is name of region
-            option.appendChild(document.createTextNode(name));
+  /**
+   * this is constructor for generating Region objects;
+   * */
+  function Region(name, y1995, y2000, y2010, y2014, y2020) {
+    //creates 6 variables
+    var name = name;
+    var y1995 = y1995;
+    var y2000 = y2000;
+    var y2010 = y2010;
+    var y2014 = y2014;
+    var y2020 = y2020;
+    //6 getters to return variables
+    this.getName = function() {
+      return name;
+    };
+    this.getY1995 = function() {
+      return y1995;
+    };
+    this.getY2000 = function() {
+      return y2000;
+    };
+    this.getY2010 = function() {
+      return y2010;
+    };
+    this.getY2014 = function() {
+      return y2014;
+    };
+    this.getY2020 = function() {
+      return y2020;
+    };
+    //function to add this object to array regions
+    this.addToArray = function(regions) {
+      regions.push(this);
+    };
+  }
 
-            //append <option> to <select>
-            $(rgnSelect).append(option);
-        });
+  /**
+   * this function reads data from JSON file,
+   * creates object instances of Region; add them to array,
+   * and calls function populateSelectElement() to create <options>
+   * */
+  function processData(data) {
+    var len = data.length;
+    //iterate through JSON data and create varibles with JSON data
+    for (var i = 0; i < len; i++) {
+      var region = data[i];
+      var name = region.regionName;
+      var y1995 = region.y1995;
+      var y2000 = region.y2000;
+      var y2010 = region.y2010;
+      var y2014 = region.y2014;
+      var y2020 = region.y2020;
+      //create Region object
+      var newRegion = new Region(name, y1995, y2000, y2010, y2014, y2020);
+      //add region to array
+      newRegion.addToArray(regions);
     }
+    //create <option> for each region in array
+    populateSelectElement(regions);
+  }
 
-    /*Creating and handling a data structure*
-     *****************************************/
-    var regions = [];
+  /*AJAX*
+    *****************************************/
+  /**
+   * this function loads JSON-encoded data from the server using a GET HTTP request;
+   * calls processData() to read file
+   * */
+  function getRegionsData() {
+    //data source:
+    //http://www.dohop.com/blog/5-charts-that-show-the-surprising-growth-of-world-tourism/
+    $.getJSON("data/regions_data.json", function(data) {
+      processData(data.regions);
+    });
+  }
 
-    /**
-     * this is constructor for generating Region objects;
-     * */
-    function Region(name, y1995, y2000, y2010, y2014, y2020) {
-        //creates 6 variables
-        var name = name;
-        var y1995 = y1995;
-        var y2000 = y2000;
-        var y2010 = y2010;
-        var y2014 = y2014;
-        var y2020 = y2020;
-        //6 getters to return variables
-        this.getName = function() {
-            return name;
-        };
-        this.getY1995 = function() {
-            return y1995;
-        };
-        this.getY2000 = function() {
-            return y2000;
-        };
-        this.getY2010 = function() {
-            return y2010;
-        };
-        this.getY2014 = function() {
-            return y2014;
-        };
-        this.getY2020 = function() {
-            return y2020;
-        };
-        //function to add this object to array regions
-        this.addToArray = function(regions) {
-            regions.push(this);
-        };
-    }
-
-    /**
-     * this function reads data from JSON file,
-     * creates object instances of Region; add them to array,
-     * and calls function populateSelectElement() to create <options>
-     * */
-    function processData(data) {
-        var len = data.length;
-        //iterate through JSON data and create varibles with JSON data
-        for (var i = 0; i < len; i++) {
-            var region = data[i];
-            var name = region.regionName;
-            var y1995 = region.y1995;
-            var y2000 = region.y2000;
-            var y2010 = region.y2010;
-            var y2014 = region.y2014;
-            var y2020 = region.y2020;
-            //create Region object
-            var newRegion = new Region(name, y1995, y2000, y2010, y2014, y2020);
-            //add region to array
-            newRegion.addToArray(regions);
-        }
-        //create <option> for each region in array
-        populateSelectElement(regions);
-    }
-
-    /*AJAX*
-     *****************************************/
-    /**
-     * this function loads JSON-encoded data from the server using a GET HTTP request;
-     * calls processData() to read file
-     * */
-    function getRegionsData() {
-        //data source:
-        //http://www.dohop.com/blog/5-charts-that-show-the-surprising-growth-of-world-tourism/
-        $.getJSON("data/regions_data.json", function(data) {
-            processData(data.regions);
-        });
-    }
-
-    getRegionsData();
+  getRegionsData();
 });
